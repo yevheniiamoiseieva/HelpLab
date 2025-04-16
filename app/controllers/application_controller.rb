3f9758844b537f0c2dd5161989d023deee35fbf7
yevheniia <moiseevaevgenya16@gmail.com>
2025-04-16 11:13:25 +0300

    feat(requests): update feed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :check_user_confirmation
   def after_sign_in_path_for(resource)
-    dashboard_path
+    root_path
   end
   protected
   def check_user_confirmation
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   get "feed/index"
   root "feed#index"
 
-  # Дашборд
   get 'dashboard', to: 'home#dashboard', as: :dashboard
   get '/about', to: 'feed#about', as: 'about'
-  # Devise: регистрация, вход, подтверждение и пр.
+
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
     registrations: 'users/registrations',
@@ -17,18 +16,14 @@ Rails.application.routes.draw do
     get '/users/sign_out', to: 'devise/sessions#destroy', as: :custom_destroy_user_session
   end
 
-  # Профіль поточного користувача
   resource :profile, only: [:show, :edit, :update]
 
-  # Профіль іншого користувача (для чату або перегляду)
   get '/profiles/:id', to: 'profiles#public_show', as: 'public_profile'
 
-  # Відгуки про користувачів
   resources :users, only: [] do
     resources :reviews, only: [:new, :create]
   end
 
-  # Запити, відповіді і чат
   resources :requests do
     resources :responses, only: [:create]
     resources :messages, only: [:index, :create]
@@ -43,7 +38,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 
-  # Уведомления
   resources :notifications, only: [:index]
 
   # PWA / System / Health
